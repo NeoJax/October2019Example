4fdd51f6f49711b6fb92d2b7eda76887087f9a61
Johnathon Lubaway <NeoJax@gmail.com>
2019-10-15 11:01:15 -0400

    First Commit on the example project

---
+++ October2019GitHubExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace October2019GitHubExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
